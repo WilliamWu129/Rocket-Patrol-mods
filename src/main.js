b87f94adb9c82c014a49b83a221860d305fc9f4a
William Wu <wwu108@ucsc.edu>
2025-01-27 20:20:48 -0800

    main updates

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+// William Wu
+// Rocket Club
+// 10 1/2 hours
+// Allow the player to control the Rocket after it's fired (1)
+// Display the time remaining (in seconds) on the screen (3) 
+// Implement an alternating two-player mode (5)
+// Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5)
+// Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses (5)
+// Implement the 'FIRE' UI text from the original game (1)
+
+
+
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
